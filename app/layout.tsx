e92591969ae44894c8e1c2035ab3e783fc7c5c7e
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-04-07 05:55:13 +0530

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,46 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { AuthProvider } from "@/context/auth-context";
+// import { Toaster } from "react-hot-toast";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Online Voting System",
+//   description: "Secure and simple online voting system",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         <AuthProvider>
+//           {children}
+//           <Toaster position="top-right" />
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-import { AuthProvider } from "@/context/auth-context";
-import { Toaster } from "react-hot-toast";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Online Voting System",
-  description: "Secure and simple online voting system",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <AuthProvider>
-          {children}
-          <Toaster position="top-right" />
-        </AuthProvider>
-      </body>
+      <ClerkProvider>
+        <body>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
