b91c93052397f83854afc0438f2dc204bc2a41fd
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-04-08 21:40:04 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,3 @@
-// import "./globals.css";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import { Inter } from "next/font/google";
-// import { Toaster } from "react-hot-toast";
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata = {
-//   title: "Simple Voting System",
-//   description: "An online voting system built with Next.js and Supabase",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <ClerkProvider>
-//       <html lang="en">
-//         <body className={inter.className}>
-//           {children}
-//           <Toaster />
-//         </body>
-//       </html>
-//     </ClerkProvider>
-//   );
-// }
-
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
